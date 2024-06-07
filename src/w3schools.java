94f2a64a07abb7ccd6108038123d9e9e4678dab8
odzhik <zhakayev.o03@gmail.com>
2024-06-08 02:11:36 +0600

    Initial commit

---
+++ src/w3schools.java
@@ -0,0 +1,236 @@
+/* Java syntax
+# 1st ex
+public class w3schools {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+2nd ex
+// This is a single-line comment
+/*  This is a multi-line comment */
+
+/* Variables
+#1st ex
+String carName = "Volvo"
+#2nd ex
+int maxSpeed = 120
+#3rd ex
+int x = 5
+int y = 10
+System.out.println(x + y)
+#4th ex
+int x = 5
+int y = 10
+int z = x + y
+System.out.println(z)
+#5th ex
+int x = 5, y = 6, z =50
+System.out.println(x+y+z)
+
+# Data types
+#1st ex
+int myNum = 9;
+float myFloatNum = 8.99f;
+char myLetter = 'A';
+boolean myBool = false;
+String myText = "Hello World";
+
+#2nd ex
+# Answer is: primitive
+
+#3rd ex
+double myDouble = 9.78d;
+int myInt = (int) myDouble;
+
+Operators
+1st ex
+System.out.println(10*5);
+
+2nd ex
+System.out.println(10/5);
+
+3rd ex
+int x = 10;
+++x;
+
+4th ex
+int x = 10;
+x+=5;
+
+Strings
+1st ex
+
+String greeting = "Hello";
+
+2nd ex
+String txt = "Hello";
+System.out.println(txt.length());
+
+3rd ex
+String txt = "Hello";
+System.out.println(txt.toUpperCase());
+
+4th ex
+String firstName = "John ";
+String lastName = "Doe";
+System.out.println(firstName+lastName);
+
+5th ex
+String firstName = "John ";
+String lastName = "Doe";
+System.out.println(firstName.concat(lastName));
+
+6th ex
+String txt = "Hello Everybody";
+System.out.println(txt.indexOf("e"));
+
+Math
+
+1st ex
+int x = 5;
+int y = 10;
+Math.max(x, y);
+
+2nd ex
+int x = 16;
+Math.sqrt(x);
+
+3rd ex
+Math.random();
+
+boolean
+
+1st ex
+
+boolean isJavaFun = true;
+boolean isFishTasty = false;
+System.out.println(isJavaFun);
+System.out.println(isFishTasty);
+
+2nd
+
+int x = 10;
+int y = 9;
+System.out.println(x > y);
+
+if-else statement
+1st ex
+int x = 50;
+int y = 10;
+if (x >y) {
+    System.out.println("Hello World");
+}
+
+2nd ex
+int x = 50;
+int y = 50;
+if (x ==y) {
+  System.out.println("Hello World");
+}
+
+3rd ex
+int x = 50;
+int y = 50;
+if
+ (x
+==
+ y) {
+  System.out.println("Yes");
+}
+else {
+  System.out.println("No");
+}
+
+4th ex
+int x = 50;
+int y = 50;
+if (x == y) {
+    System.out.println("1");
+} else if (x > y) {
+  System.out.println("2");
+} else {
+  System.out.println("3");
+}
+
+5th ex
+int time = 20;
+String result = (time < 18)?"Good day." :"Good evening.";
+System.out.println(result);
+
+switch
+
+1 ex
+int day = 2;
+switch (day) {
+  case1:
+    System.out.println("Saturday");
+    break;
+  case2:
+    System.out.println("Sunday");
+    break;
+}
+
+2 ex
+
+int day = 4;
+switch (day) {
+  case1:
+    System.out.println("Saturday");
+    break;
+  case2:
+    System.out.println("Sunday");
+    break;
+  default:
+    System.out.println("Weekend");
+}
+
+loops
+
+1 ex
+int i = 1;
+while(i < 6) {
+  System.out.println(i);
+  i++;
+}
+
+2 ex
+int i = 1;
+do {
+  System.out.println(i);
+  i++;
+}
+while (i < 6);
+
+3 ex
+
+
+for (int i = 0; i < 5; i++) {
+  System.out.println("Yes");
+}
+
+4 ex
+String[] cars = {"Volvo", "BMW", "Ford"};
+for (String i : cars) {
+  System.out.println(i);
+
+5 ex
+for (int i = 0; i < 10; i++) {
+  if (i == 5) {
+    break;
+  }
+  System.out.println(i);
+}
+6 ex
+for (int i = 0; i < 10; i++) {
+  if (i == 4) {
+    continue;
+  }
+  System.out.println(i);
+}
+
+
+
+ */
+
+
+
